35a0b8c6ec4c13a718d867498a4b8a9e7798321e
Francisco Adasme <francisco.adasme@gmail.com>
2022-04-28 18:12:02 -0400

    Parameter types no longer save atom types

--- src/mm/params.cr
+++ src/mm/params.cr
@@ -1,18 +1,10 @@
 module MM
   module ParameterType
-    abstract def atom_types : Indexable(AtomType)
 
-    def to_s(io : IO) : Nil
-      atom_types.join('-', &.name)
-    end
-
-    def typenames : Indexable(String)
-      atom_types.map(&.name)
     end
   end
 
   record BondType,
-    atom_types : {AtomType, AtomType},
     force_constant : Float64,
     eq_value : Float64 do
     include ParameterType
@@ -20,7 +12,6 @@ module MM
   end
 
   record AngleType,
-    atom_types : {AtomType, AtomType, AtomType},
     force_constant : Float64,
     eq_value : Float64 do
     include ParameterType
@@ -28,7 +19,6 @@ module MM
   end
 
   record DihedralType,
-    atom_types : {AtomType, AtomType, AtomType, AtomType},
     multiplicity : Int32,
     force_constant : Float64,
     phase : Float64 do
@@ -37,7 +27,6 @@ module MM
   end
 
   record ImproperType,
-    atom_types : {AtomType, AtomType, AtomType, AtomType},
     force_constant : Float64,
     eq_value : Float64 do
     include ParameterType
