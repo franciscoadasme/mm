0437c72c1d1823e9e11983e10b3338531f90b0ef
Francisco Adasme <francisco.adasme@gmail.com>
2022-04-28 18:16:54 -0400

    Read CHARMM parameter file

--- spec/charmm_spec.cr
+++ spec/charmm_spec.cr
@@ -4,7 +4,7 @@ describe MM::CHARMM do
   describe ".read_topology" do
     it "reads a topology file" do
       params = MM::CHARMM.read_topology "spec/data/top_opls_aam_M.inp"
-      params.atom_types.size.should eq 79
+      params.atoms.size.should eq 79
       params.residues.size.should eq 25
       params.patches.size.should eq 18
 
@@ -24,6 +24,82 @@ describe MM::CHARMM do
       patch.delete_atoms.should eq ["1HG1", "2HG1"]
       patch.bonds.size.should eq 1
       patch.bonds.count(&.order.==(2)).should eq 0
+    end
+  end
+
+  describe ".load_parameters" do
+    it "reads a parameter file" do
+      params = MM::CHARMM.read_topology "spec/data/top_opls_aam_M.inp"
+      MM::CHARMM.load_parameters params, "spec/data/par_opls_aam_M.inp"
+
+      params.bonds.size.should eq 313
+
+      bond = params.bonds[{"C223", "C267"}]?.should_not be_nil
+      bond.force_constant.should eq 317
+      bond.eq_value.should eq 1.522
+      bond.penalty.should eq 0.0
+      bond.comment.should eq "phosphorylated C-terminal Gly, adm jr."
+
+      params.angles.size.should eq 843
+
+      params.angles[{"C136", "C224", "C267"}]?.should be_nil # commented
+
+      angle = params.angles[{"HT", "OT", "HT"}]?.should_not be_nil
+      angle.force_constant.should eq 55
+      angle.eq_value.should eq 104.52
+      angle.penalty.should eq 0.0
+      angle.comment.should be_nil
+
+      angle = params.angles[{"C223", "C267", "O268"}]?.should_not be_nil
+      angle.force_constant.should eq 70
+      angle.eq_value.should eq 108
+      angle.penalty.should eq 264.5
+      angle.comment.should eq "protonated C-terminal Gly, adm jr."
+
+      params.dihedrals.size.should eq 1630
+
+      dihedrals = params.dihedrals[{nil, "C145", "C145", nil}]?.should_not be_nil
+      dihedrals.size.should eq 1
+      dihedrals[0].force_constant.should eq 3.625
+      dihedrals[0].multiplicity.should eq 2
+      dihedrals[0].eq_value.should eq 180
+      dihedrals[0].penalty.should eq 0.0
+      dihedrals[0].comment.should be_nil
+
+      dihedrals = params.dihedrals[{"C505", "C224", "C235", "N238"}]?.should_not be_nil
+      dihedrals.size.should eq 3
+      dihedrals[0].force_constant.should eq 0.8895
+      dihedrals[0].multiplicity.should eq 1
+      dihedrals[0].eq_value.should eq 0
+      dihedrals[0].penalty.should eq 0.0
+      dihedrals[0].comment.should be_nil
+      dihedrals[1].force_constant.should eq 0.2095
+      dihedrals[1].multiplicity.should eq 2
+      dihedrals[1].eq_value.should eq 180
+      dihedrals[1].penalty.should eq 0.0
+      dihedrals[1].comment.should be_nil
+      dihedrals[2].force_constant.should eq -0.0550
+      dihedrals[2].multiplicity.should eq 3
+      dihedrals[2].eq_value.should eq 0
+      dihedrals[2].penalty.should eq 0.0
+      dihedrals[2].comment.should be_nil
+
+      params.impropers.size.should eq 210
+
+      improper = params.impropers[{"O268", "C267", "C223", "O269"}]?.should_not be_nil
+      improper.force_constant.should eq 10.5
+      improper.eq_value.should eq 180
+      improper.penalty.should eq 0.0
+      improper.comment.should eq "Gly"
+
+      params.atoms.values.count(&.lj).should eq 78
+      atom = params.atoms["CLA"]?.should_not be_nil
+      lj = atom.lj.should_not be_nil
+      lj.epsilon.should eq -0.71
+      lj.rmin.should eq 4.5122974
+      lj = atom.lj14.should_not be_nil
+      lj.epsilon.should eq -0.71
+      lj.rmin.should eq 4.5122974
     end
   end
 end
--- src/mm/charmm.cr
+++ src/mm/charmm.cr
@@ -1,4 +1,108 @@
 module MM::CHARMM
+  PARAM_PENALTY_REGEX = /\s*penalty\s*=\s*([\d\.]+)/i
+  WILDCARD_TYPE_NAME  = "X"
+
+  def self.load_parameters(params : ParameterSet, parfile : Path | String) : Nil
+    File.open(parfile) do |io|
+      section = nil
+      line = io.gets
+      while line
+        content, _, comment = line.partition(/\s*[!\*]\s*/)
+        tokens = content.split
+        comment = comment.presence
+        if comment && comment =~ PARAM_PENALTY_REGEX
+          penalty = $1.to_f? || raise "Invalid penalty"
+          comment = comment.gsub PARAM_PENALTY_REGEX, ""
+        else
+          penalty = 0.0
+        end
+
+        case {section, tokens[0]?.try(&.upcase)}
+        when {_, Nil}
+          # comment line, do nothing
+        when {_, "ATOM"}
+          section = "atoms"
+        when {_, "BOND"}
+          section = "bonds"
+        when {_, "ANGLE"}, {_, "THETA"}
+          section = "angles"
+        when {_, "DIHEDRAL"}, {_, "PHI"}
+          section = "dihedrals"
+        when {_, "IMPROPER"}, {_, "IMPHI"}
+          section = "impropers"
+        when {_, "NONBONDED"}
+          section = "nonbonded"
+        when {_, "CMAP"}, {_, "NBFIX"}, {_, "HBOND"}, {_, "THOLE"}, {_, "END"}
+          section = nil
+        when {"atoms", "MASS"}
+          name = tokens[2]
+          mass = tokens[3].to_f? || raise "Invalid mass"
+          element = tokens[4].try { |sym| Chem::PeriodicTable[sym] }
+          # Use element with the closest mass if missing
+          element ||= Chem::PeriodicTable.elements.min_by &.mass.-(mass).abs
+          params << AtomType.new(name, element, mass, comment: comment)
+        when {"bonds", _}
+          type1 = tokens[0].upcase
+          type2 = tokens[1].upcase
+          force_constant = tokens[2].to_f? || raise "Invalid force constant"
+          eq_value = tokens[3].to_f? || raise "Invalid equilibrium value"
+          new_type = BondType.new force_constant, eq_value, penalty, comment
+          params[{type1, type2}] = new_type
+        when {"angles", _}
+          type1 = tokens[0].upcase
+          type2 = tokens[1].upcase
+          type3 = tokens[2].upcase
+          force_constant = tokens[3].to_f? || raise "Invalid force constant"
+          eq_value = tokens[4].to_f? || raise "Invalid equilibrium value"
+          new_type = AngleType.new force_constant, eq_value, penalty, comment
+          params[{type1, type2, type3}] = new_type
+        when {"dihedrals", _}
+          type1 = tokens[0].upcase
+          type1 = nil if type1 == WILDCARD_TYPE_NAME
+          type2 = tokens[1].upcase
+          type3 = tokens[2].upcase
+          type4 = tokens[3].upcase
+          type4 = nil if type4 == WILDCARD_TYPE_NAME
+          force_constant = tokens[4].to_f? || raise "Invalid force constant"
+          multiplicity = tokens[5].to_i? || raise "Invalid multiplicity"
+          eq_value = tokens[6].to_f? || raise "Invalid equilibrium value"
+          new_type = DihedralType.new multiplicity, force_constant, eq_value, penalty, comment
+          params[{type1, type2, type3, type4}] = new_type
+        when {"impropers", _}
+          type1 = tokens[0].upcase
+          type2 = tokens[1].upcase
+          type2 = nil if type2 == WILDCARD_TYPE_NAME
+          type3 = tokens[2].upcase
+          type3 = nil if type3 == WILDCARD_TYPE_NAME
+          type4 = tokens[3].upcase
+          force_constant = tokens[4].to_f? || raise "Invalid force constant"
+          eq_value = tokens[6].to_f? || raise "Invalid equilibrium value"
+          new_type = ImproperType.new force_constant, eq_value, penalty, comment
+          params[{type1, type2, type3, type4}] = new_type
+        when {"nonbonded", _}
+          epsilon = tokens[2].to_f?
+          rmin = tokens[3].to_f?.try &.*(2)
+          if epsilon && rmin
+            typename = tokens[0].upcase
+            if (epsilon14 = tokens[5]?) && (rmin14 = tokens[6]?)
+              epsilon14 = epsilon14.to_f? || raise "Invalid epsilon"
+              rmin14 = rmin14.to_f?.try(&.*(2)) || raise "Invalid Rmin"
+              if atom_type = params.atoms[typename]?
+                atom_type.lj = LennardJones.new(epsilon, rmin, comment)
+                atom_type.lj14 = LennardJones.new(epsilon14, rmin14, comment)
+              else
+                raise "Unknown atom type #{typename}"
+              end
+            end
+          else # probably header
+            # ignore for now
+          end
+        end
+        line = io.gets
+      end
+    end
+  end
+
   def self.load_topology(params : ParameterSet, topfile : Path | String) : Nil
     default_first_patch = default_last_patch = nil
     File.open(topfile) do |io|
@@ -88,6 +192,12 @@ module MM::CHARMM
         end
         line = io.gets
       end
+    end
+  end
+
+  def self.read_parameters(parfile : Path | String) : ParameterSet
+    ParameterSet.new.tap do |params|
+      load_parameters params, parfile
     end
   end
 
