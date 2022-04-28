76eda40c1916771e31d95ae0e5e8ddcf8ca40892
Francisco Adasme <francisco.adasme@gmail.com>
2022-04-28 18:13:52 -0400

    Refactor ParameterType mixin

--- src/mm/params.cr
+++ src/mm/params.cr
@@ -1,31 +1,43 @@
 module MM
   module ParameterType
+    getter force_constant : Float64
+    getter eq_value : Float64
+    getter penalty : Float64 = 0.0
+    getter comment : String?
 
+    def initialize(
+      @force_constant : Float64,
+      @eq_value : Float64,
+      @penalty : Float64 = 0.0,
+      @comment : String? = nil
+    )
     end
   end
 
-  record BondType,
-    force_constant : Float64,
-    eq_value : Float64 do
+  struct BondType
     include ParameterType
   end
 
-  record AngleType,
-    force_constant : Float64,
-    eq_value : Float64 do
+  struct AngleType
     include ParameterType
   end
 
-  record DihedralType,
-    multiplicity : Int32,
-    force_constant : Float64,
-    phase : Float64 do
+  struct DihedralType
     include ParameterType
+
+    getter multiplicity : Int32
+
+    def initialize(
+      @multiplicity : Int32,
+      @force_constant : Float64,
+      @eq_value : Float64,
+      @penalty : Float64 = 0.0,
+      @comment : String? = nil
+    )
+    end
   end
 
-  record ImproperType,
-    force_constant : Float64,
-    eq_value : Float64 do
+  struct ImproperType
     include ParameterType
   end
 end
