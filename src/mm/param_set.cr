53eb3a7fc12c507514296d2e687bde3ed8354dd3
Francisco Adasme <francisco.adasme@gmail.com>
2022-05-19 15:09:37 -0400

    Refactor macro code

--- src/mm/param_set.cr
+++ src/mm/param_set.cr
@@ -37,10 +37,15 @@ class MM::ParameterSet
     params
   end
 
-  {% for name in %w(angle atom bond improper patch residue) %}
-    {% type = (name == "patch" ? "Patch" : "#{name.camelcase.id}Type") %}
+  {% for name in %w(angle atom bond dihedral improper patch residue) %}
     {% plural_name = (name == "patch" ? "patches" : "#{name.id}s") %}
+    {% type = (name == "patch" ? "Patch" : "#{name.camelcase.id}Type") %}
     {% key = %w(atom patch residue).includes?(name) ? "name" : "typenames" %}
+    {% return_type = name == "dihedral" ? "Array::View(#{type.id})" : type %}
+
+    def {{plural_name.id}} : Array::View({{return_type.id}})
+      @{{plural_name.id}}.view
+    end
 
     def <<({{name.id}}_t : {{type.id}}) : self
       if i = @{{plural_name.id}}.index(&.==({{name.id}}_t.{{key.id}}))
@@ -50,6 +55,55 @@ class MM::ParameterSet
       end
       self
     end
+
+    {% if %w(angle bond dihedral improper).includes?(name) %}
+      # Returns the {{name.id}} parameter type associated with *{{name.id}}*.
+      # Raises `KeyError` if the parameter does not exists.
+      #
+      # It uses the typenames of the involved atoms as key to fetch the
+      # parameter type. Raises `ArgumentError` if any atom does not have a
+      # assigned type (`nil`).
+      def []({{name.id}} : Chem::{{name.id.camelcase}}) : {{return_type.id}}
+        self[{{name.id}}]? || raise KeyError.new("Missing parameter for #{{{name.id}}}")
+      end
+
+      # Returns the {{name.id}} parameter type associated with *{{name.id}}* if
+      # exists, else `nil`.
+      #
+      # It uses the typenames of the involved atoms as key to fetch the
+      # parameter type. Raises `ArgumentError` if any atom does not have a
+      # assigned type (`nil`).
+      def []?({{name.id}} : Chem::{{name.id.camelcase}}) : {{return_type.id}}?
+        typenames = {{name.id}}.atoms.map { |atom|
+          atom.type || raise ArgumentError.new("#{atom} has no type")
+        }
+        {{name.id}}? typenames
+      end
+
+      def fuzzy_search({{name.id}} : Chem::{{name.id.camelcase}}) : Array({{return_type.id}})
+        pattern = {{name.id}}.atoms.map do |atom|
+          typename = atom.type || raise ArgumentError.new("#{atom} has no type")
+          atom_type = atom?(typename) || raise KeyError.new("Unknown atom type #{typename}")
+          resname = atom.residue.name
+          restype = residue?(resname) || raise KeyError.new("Unknown residue type #{resname}")
+          if typename == restype.atoms[atom.name]?.try(&.typename)
+            typename
+          else # atom was changed or added by a patch
+            atom_type.element
+          end
+        end
+
+        @{{name.id}}s.select do |{{name.id}}|
+          {{name.id}}{% if name == "dihedral" %}[0]{% end %}.typenames.zip(pattern).all? { |typename, atom_pattern|
+            if atom_type = typename.try { |typename| atom?(typename) }
+              atom_type.matches?(atom_pattern)
+            else # nil signals any atom (wildcard)
+              true
+            end
+          }
+        end{% if name == "dihedral" %}.map(&.view){% end %}
+      end
+    {% end %}
   {% end %}
 
   def <<(dihedral_t : DihedralType) : self
@@ -70,56 +124,16 @@ class MM::ParameterSet
     self
   end
 
-  {% for type in %w(bond angle dihedral improper).map(&.id) %}
-    {% return_type = "#{type.camelcase}Type".id %}
-    {% return_type = "Array::View(#{return_type})" if type == "dihedral" %}
-
-    # Returns the {{type}} parameter type associated with *{{type}}*.
-    # Raises `KeyError` if the parameter does not exists.
-    #
-    # It uses the typenames of the involved atoms as key to fetch the
-    # parameter type. Raises `ArgumentError` if any atom does not have a
-    # assigned type (`nil`).
-    def []({{type}} : Chem::{{type.camelcase}}) : {{return_type.id}}
-      self[{{type}}]? || raise KeyError.new("Missing parameter for #{{{type}}}")
-    end
-
-    # Returns the {{type}} parameter type associated with *{{type}}* if
-    # exists, else `nil`
-    #
-    # It uses the typenames of the involved atoms as key to fetch the
-    # parameter type. Raises `ArgumentError` if any atom does not have a
-    # assigned type (`nil`).
-    def []?({{type}} : Chem::{{type.camelcase}}) : {{return_type.id}}?
-      typenames = {{type}}.atoms.map { |atom|
-        atom.type || raise ArgumentError.new("#{atom} has no type")
-      }
-      {{type}}? typenames
-    end
-  {% end %}
-
   def angle?(typenames : {String, String, String}) : AngleType?
     @angles.find &.===(typenames)
-  end
-
-  def angles : Array::View(AngleType)
-    @angles.view
   end
 
   def atom?(name : String) : AtomType?
     @atoms.find &.name.==(name.upcase)
   end
 
-  def atoms : Array::View(AtomType)
-    @atoms.view
-  end
-
   def bond?(typenames : {String, String}) : BondType?
     @bonds.find &.===(typenames)
-  end
-
-  def bonds : Array::View(BondType)
-    @bonds.view
   end
 
   def detect_missing(top : Chem::Topology) : Array(C)
@@ -138,60 +152,19 @@ class MM::ParameterSet
     @dihedrals.find(&.first.===(typenames)).try(&.view)
   end
 
-  def dihedrals : Array::View(Array(DihedralType))
-    @dihedrals.view
+  def dihedrals : Array::View(Array::View(DihedralType))
+    @dihedrals.map(&.view).view
   end
 
   def improper?(typenames : {String, String?, String?, String}) : ImproperType?
     @impropers.find &.===(typenames)
   end
 
-  def impropers : Array::View(ImproperType)
-    @impropers.view
-  end
-
   def patch?(name : String) : Patch?
     @patches.find &.name.==(name.upcase)
-  end
-
-  def patches : Array::View(Patch)
-    @patches.view
   end
 
   def residue?(name : String) : ResidueType?
     @residues.find &.name.==(name.upcase)
   end
-
-  def residues : Array::View(ResidueType)
-    @residues.view
-  end
-
-  {% for type in %w(bond angle dihedral improper).map(&.id) %}
-    {% return_type = type == "dihedral" ? Array(DihedralType) : "#{type.camelcase}Type".id %}
-    def fuzzy_search(
-      {{type}} : Chem::{{type.camelcase}}
-    ) : Array({{return_type}})
-      pattern = {{type}}.atoms.map do |atom|
-        typename = atom.type || raise ArgumentError.new("#{atom} has no type")
-        atom_type = atom?(typename) || raise KeyError.new("Unknown atom type #{typename}")
-        resname = atom.residue.name
-        restype = residue?(resname) || raise KeyError.new("Unknown residue type #{resname}")
-        if typename == restype.atoms[atom.name]?.try(&.typename)
-          typename
-        else # atom was changed or added by a patch
-          atom_type.element
-        end
-      end
-
-      @{{type}}s.select do |{{type}}|
-        {{type}}{% if type == "dihedral" %}[0]{% end %}.typenames.zip(pattern).all? { |typename, atom_pattern|
-          if atom_type = typename.try { |typename| atom?(typename) }
-            atom_type.matches?(atom_pattern)
-          else # nil signals any atom (wildcard)
-            true
-          end
-        }
-      end
-    end
-  {% end %}
 end
